e4501182e1be00de71ddfe376f35b6fc159b1bdb
Andrew Nguyen <andrew.nguyen42@gmail.com>
2015-09-27 12:04:29 -0500

    got rid of some garbage

--- 02 Data Wrangling/groupBy.R
+++ 02 Data Wrangling/groupBy.R
@@ -1,12 +1,5 @@
 df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="SELECT * from EXOPLANETS WHERE PLANETDISCMETH IS NOT NULL AND DATE_0 IS NOT NULL"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_cdt932', PASS='orcl_cdt932', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
-df %>% select(MICROLENSING,IMAGING,TIMING_0,ASTROMETRY)
-
-
-byDataMethod <- group_by(df, PLANETDISCMETH, DATE_0)
-
-
-
 discoveryTypeYearDF <- df %.%
   group_by(DATE_0, PLANETDISCMETH) %.%
   summarise(discoveryCount = length(PLANETDISCMETH))
