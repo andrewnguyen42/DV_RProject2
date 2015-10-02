01b3bde997b78a8f4e1e1343fb46e1089969e759
Andrew Nguyen <andrew.nguyen42@gmail.com>
2015-10-01 22:08:38 -0500

    used filter instead of select statement

--- 02 Data Wrangling/groupBy.R
+++ 02 Data Wrangling/groupBy.R
@@ -1,6 +1,8 @@
-df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="SELECT * from EXOPLANETS WHERE PLANETDISCMETH IS NOT NULL AND DATE_0 IS NOT NULL"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_cdt932', PASS='orcl_cdt932', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="SELECT * from EXOPLANETS"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_cdt932', PASS='orcl_cdt932', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
-discoveryTypeYearDF <- df %.%
+s <- select(df, PLANETDISCMETH, DATE_0)
+
+discoveryTypeYearDF <- filter(df, PLANETDISCMETH != "null", DATE_0 != "null") %.%
   group_by(DATE_0, PLANETDISCMETH) %.%
   summarise(discoveryCount = length(PLANETDISCMETH))
 
