4fa8b827840c4756acf0e96c5a240b4e3965dcd5
Andrew Nguyen <andrew.nguyen42@gmail.com>
2015-10-01 22:44:39 -0500

    fixed up some labels on group by and added the kepler facets visualization

--- 02 Data Wrangling/keplerFacets.R
+++ 02 Data Wrangling/keplerFacets.R
@@ -1,11 +1,11 @@
 df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="SELECT * from EXOPLANETS"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_cdt932', PASS='orcl_cdt932', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
-discoveryTypeYearDF <- df%.% filter(PLANETDISCMETH != "null", DATE_0 != "null") %.%
-  group_by(DATE_0, PLANETDISCMETH) %.%
-  summarise(discoveryCount = length(PLANETDISCMETH))
+keplerDiscoveriesDF <- df %.% select(KDE, DATE_0) %.%filter (DATE_0 != 'null') %.%
+  group_by(DATE_0, KDE) %.% summarise(discoveryCount = length(KDE)) %.% arrange(DATE_0)
 
 
-ggplot(discoveryTypeYearDF, aes(x=as.Date(DATE_0, format = '%Y'),y=discoveryCount,fill=PLANETDISCMETH))+
+ggplot(keplerDiscoveriesDF, aes(x=as.Date(DATE_0, format = '%Y'),y=discoveryCount))+
   geom_bar(stat="identity")+
-labs(title='Discovery Type VS Year') +
-  labs(x="Discovery Date", y=paste("Planet Discovery Method")) 
+  labs(title='Kepler vs. Non-Kepler Discoveries') +
+  labs(x="Discovery Date", y=paste("Planet Discoveries")) +
+  facet_grid(. ~ KDE)
